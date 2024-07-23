89b948e0dc458cbf437946f74f50f865e9c4ced3
stalyn <smtihsly@gmail.com>
2024-07-22 19:59:29 -0500

    whitenoise y diseño movil

--- san_andres/settings.py
+++ san_andres/settings.py
@@ -25,9 +25,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://voyaventura-san-andres.up.railway.app',
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://voyaventura-san-andres.up.railway.app',
+    
+# ]
 
 # Application definition
 
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # whitnoise añadido
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     # Aplicaciones personalizadas
     'alojamiento',
@@ -58,7 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-   
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,15 +149,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
