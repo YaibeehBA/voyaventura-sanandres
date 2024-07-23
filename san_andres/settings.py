dbfedbe32d9a0d61fa8508f2b95fcee4878adde4
stalyn <smtihsly@gmail.com>
2024-07-22 20:03:55 -0500

    csrf

--- san_andres/settings.py
+++ san_andres/settings.py
@@ -25,10 +25,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://voyaventura-san-andres.up.railway.app',
+CSRF_TRUSTED_ORIGINS = [
+    'https://voyaventura-san-andres.up.railway.app',
     
-# ]
+]
 
 # Application definition
 
