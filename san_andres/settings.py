30bd168fc47673b9671a86f9dc78b5f39d984eb9
stalyn <smtihsly@gmail.com>
2024-07-24 02:08:17 -0500

    debug variable de entorno

--- san_andres/settings.py
+++ san_andres/settings.py
@@ -17,8 +17,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
-
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     'voyaventura-san-andres.up.railway.app',
